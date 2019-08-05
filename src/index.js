29a42287036757e1c50b011e1e6dd22124b5e18f
Ben Russert <benrussert@gmail.com>
2019-08-05 14:31:11 -0600

    Drop mobx in favor of react hooks

--- src/index.js
+++ src/index.js
@@ -1,78 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
-import { observable, computed, autorun, when, action } from "mobx";
-import { observer } from "mobx-react";
 import "./styles.css";
 
-class OrderLine {
-  @observable price = 4.0;
-  @observable amount = 1;
+function App({ store }) {
+  const [count, setCount] = useState(1);
+  const [price, setPrice] = useState(4.0);
 
-  @computed get total() {
-    return this.price * this.amount;
-  }
+  // Note: if you have an expensive calculation here and want it optimized like  mobx `computed`,
+  // you can use the React useMemo api to only recalculate whenever `count` or `price` changes
+  // IMHO: It's better not to add useMemo complexity unless it makes a noticiable difference to the user (rare)
+  const cost = (price * count).toFixed(2);
 
-  @action setPrice({ target }) {
-    // takes an event object
-    const newAmount = Number(target.value);
-    console.log(newAmount, typeof newAmount);
-    if (typeof newAmount === "number") {
-      this.price = newAmount;
-    }
-  }
-}
+  // Our side effects will go here, but
+  // you could extract them to custom hooks easily if you want to reuse or just organize them nicer.
 
-const myStore = new OrderLine();
+  useEffect(
+    function doThisWheneverCountChanges() {
+      console.log("Amount is now: ", count);
+      if (count == 5) alert("Congratulations on 5 clicks!");
+    },
+    [count]
+  );
 
-const ObserverApp = observer(function App({ store }) {
+  useEffect(
+    function doThisWheneverPriceChanges() {
+      console.log("Price is now: ", price);
+    },
+    [price]
+  );
+
   return (
     <div className="App">
-      <button onClick={() => (store.amount += 1)}>Increment count</button>
+      <button onClick={() => setCount(count + 1)}>Increment count</button>
       <h1>Total Count</h1>
       <span role="img" aria-label="One box item for each count">
-        {"📦".repeat(store.amount)}
+        {"📦".repeat(count)}
       </span>
-      <h2>{store.amount} </h2>
+      <h2>{count} </h2>
       <h1>Total Price:</h1>
       <label>
         <input
-          defaultValue={myStore.price}
-          // why would it not work just like this? binding `this`.
-          // onChange={myStore.setPrice}
-          onChange={e => myStore.setPrice(e)}
+          defaultValue={price}
+          onChange={({ target }) => setPrice(target.value)}
         />
       </label>
-      <h2>${store.price.toFixed(2)}</h2>
+      <h2>${price}</h2>
       <h1>Total Cost:</h1>
-      <h2>${store.total.toFixed(2)}</h2>
+      <h2>${cost}</h2>
     </div>
   );
-});
-
-window.myStore = myStore;
-
-function doThisWheneverAmountChanges() {
-  console.log("Amount is now: ", myStore.amount);
 }
-
-function doThisWheneverPriceChanges() {
-  console.log("Price is now: ", myStore.price);
-}
-
-// call amountDisposer() when you want to stop
-const amountDisposer = autorun(doThisWheneverAmountChanges);
-const priceDisposer = autorun(doThisWheneverPriceChanges);
-
-const disposeWhen = when(
-  () => myStore.amount >= 5,
-  () => {
-    alert("Congratulations on 5 clicks!");
-    // uncomment to stop logging after 5
-    // amountDisposer();
-    // this disposes the when, which probably isnt important...
-    disposeWhen();
-  }
-);
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<ObserverApp store={myStore} />, rootElement);
+ReactDOM.render(<App />, rootElement);
