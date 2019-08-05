727d6d7ed40e1f632c16f9f9b08f6fe5790f5f3c
Ben Russert <benrussert@gmail.com>
2019-08-05 15:50:35 -0600

    Break apart into components

--- src/index.js
+++ src/index.js
@@ -2,18 +2,7 @@ import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
 
-function App() {
-  const [count, setCount] = useState(1);
-  const [price, setPrice] = useState(4.0);
-
-  // Note: if you have an expensive calculation here and want it optimized like  mobx `computed`,
-  // you can use the React useMemo api to only recalculate whenever `count` or `price` changes
-  // IMHO: It's better not to add useMemo complexity unless it makes a noticiable difference to the user (rare)
-  const cost = (price * count).toFixed(2);
-
-  // Our side effects will go here, but
-  // you could extract them to custom hooks easily if you want to reuse or just organize them nicer.
-
+function CountTracker({ count, setCount }) {
   useEffect(
     function doThisWheneverCountChanges() {
       console.log("Amount is now: ", count);
@@ -21,22 +10,27 @@ function App() {
     },
     [count]
   );
-
-  useEffect(
-    function doThisWheneverPriceChanges() {
-      console.log("Price is now: ", price);
-    },
-    [price]
-  );
-
   return (
-    <div className="App">
+    <>
       <button onClick={() => setCount(count + 1)}>Increment count</button>
       <h1>Total Count</h1>
       <span role="img" aria-label="One box item for each count">
         {"📦".repeat(count)}
       </span>
       <h2>{count} </h2>
+    </>
+  );
+}
+
+function PriceTracker({ price, setPrice }) {
+  useEffect(
+    function doThisWheneverPriceChanges() {
+      console.log("Price is now: ", price);
+    },
+    [price]
+  );
+  return (
+    <>
       <h1>Total Price:</h1>
       <label>
         <input
@@ -45,8 +39,33 @@ function App() {
         />
       </label>
       <h2>${price}</h2>
+    </>
+  );
+}
+
+function TotalCostTracker({ price, count }) {
+  // Note: if you have an expensive calculation here and want it optimized like  mobx `computed`,
+  // you can use the React useMemo api to only recalculate whenever `count` or `price` changes
+  // IMHO: It's better not to add useMemo complexity unless it makes a noticiable difference to the user (rare)
+  const cost = (price * count).toFixed(2);
+
+  return (
+    <>
       <h1>Total Cost:</h1>
       <h2>${cost}</h2>
+    </>
+  );
+}
+
+function App() {
+  const [count, setCount] = useState(1);
+  const [price, setPrice] = useState(4.0);
+
+  return (
+    <div className="App">
+      <CountTracker count={count} setCount={setCount} />
+      <PriceTracker price={price} setPrice={setPrice} />
+      <TotalCostTracker price={price} count={count} />
     </div>
   );
 }
